7aff761e6dd9647eb118cfc3bff6efef54d7b0da
Cory Simmons <csimmonswork@gmail.com>
2024-10-30 14:31:12 -0400

    Use tailwindui template

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useState } from 'react'
+import clsx from 'clsx'
+
+import { TinyWaveFormIcon } from '@/components/TinyWaveFormIcon'
+
+export function AboutSection(props: React.ComponentPropsWithoutRef<'section'>) {
+  let [isExpanded, setIsExpanded] = useState(false)
+
+  return (
+    <section {...props}>
+      <h2 className="flex items-center font-mono text-sm font-medium leading-7 text-slate-900">
+        <TinyWaveFormIcon
+          colors={['fill-violet-300', 'fill-pink-300']}
+          className="h-2.5 w-2.5"
+        />
+        <span className="ml-2.5">About</span>
+      </h2>
+      <p
+        className={clsx(
+          'mt-2 text-base leading-7 text-slate-700',
+          !isExpanded && 'lg:line-clamp-4',
+        )}
+      >
+        A podcast that dives into the mind of one
+        endlessly curious guy. It's a journey led by curiosity, tackling big
+        questions, small wonders, and everything in between. From science and
+        culture to life's everyday mysteries, Donegan brings his unique
+        perspective to explore the world around us, one fascinating conversation
+        at a time. Tune in to exchange ideas, uncover insights, and satisfy that
+        natural human itch to know a little bit more about everything.
+      </p>
+      {!isExpanded && (
+        <button
+          type="button"
+          className="mt-2 hidden text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 lg:inline-block"
+          onClick={() => setIsExpanded(true)}
+        >
+          Show more
+        </button>
+      )}
+    </section>
+  )
+}
